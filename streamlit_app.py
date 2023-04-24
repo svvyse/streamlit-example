df239aaa768c8dfdaf7acb73344c7d5613a5a23a
Salvador Lopez <46700662+svvyse@users.noreply.github.com>
2023-04-24 13:08:46 -0600

    Actualizar streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,8 @@ dataframe = pd.DataFrame(
     columns=('col %d' % i for i in range(20)))
 
 st.dataframe(dataframe.style.highlight_max(axis=0))
-chart_data = pd.DataFrame( np.random.randn(20, 3), columns=['a', 'b', 'c']) st.line_chart(chart_data)
+chart_data = pd.DataFrame( np.random.randn(20, 3), columns=['a', 'b', 'c'])
+st.line_chart(chart_data)
 
  # connect to snowflake 
  #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
