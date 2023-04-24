1e2f5f3e663513a9d5fa279535915d21565155f3
Salvador Lopez <46700662+svvyse@users.noreply.github.com>
2023-04-24 15:10:52 -0600

    Actualizar streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,10 @@ st.map(map_data)
 x = st.slider('x') # 👈 this is a widget
 st.write(x, 'squared is', x * x)
 
+
+st.text_input("Your name", key="name") 
+# You can access the value at any point with: st.session_state.name
+
  # connect to snowflake 
  #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
  #my_cur = my_cnx.cursor() 
