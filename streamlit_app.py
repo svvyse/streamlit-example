b34a6a29ef2f5b24bfff27aa8cda5fb7b301e1f2
Salvador Lopez <46700662+svvyse@users.noreply.github.com>
2023-04-24 13:05:40 -0600

    Actualizar streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ dataframe = pd.DataFrame(
     columns=('col %d' % i for i in range(20)))
 
 st.dataframe(dataframe.style.highlight_max(axis=0))
+st.table(dataframe)
 
  # connect to snowflake 
  #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
