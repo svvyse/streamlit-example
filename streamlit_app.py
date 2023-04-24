9c9bd06ed0dadc47506cd1532571177c53996565
Salvador Lopez <46700662+svvyse@users.noreply.github.com>
2023-04-24 10:32:18 -0600

    Actualizar streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,33 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+ import streamlit 
+ import snowflake.connector 
+ import pandas 
+  
+ streamlit.title('Papeleria') 
+ # connect to snowflake 
+ #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
+ #my_cur = my_cnx.cursor() 
+ # run a snowflake query and put it all in a var called my_catalog 
+ my_cur.execute("select color_or_style from catalog_for_website") 
+ my_catalog = my_cur.fetchall() 
+ # put the dafta into a dataframe 
+ df = pandas.DataFrame(my_catalog) 
+ # temp write the dataframe to the page so I Can see what I am working with 
+ # streamlit.write(df) 
+ # put the first column into a list 
+ color_list = df[0].values.tolist() 
+ # print(color_list) 
+ # Let's put a pick list here so they can pick the color 
+ option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list)) 
+ # We'll build the image caption now, since we can 
+ product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!' 
+ # use the option selected to go back and get all the info from the database 
+ my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';") 
+ df2 = my_cur.fetchone() 
+ streamlit.image( 
+ df2[0], 
+ width=400, 
+ caption= product_caption 
+ ) 
+ streamlit.write('Price: ', df2[1]) 
+ streamlit.write('Sizes Available: ',df2[2]) 
+ streamlit.write(df2[3])
