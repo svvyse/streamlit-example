894260cadf9c0e220eb52eba35d117ed82c8d301
Salvador Lopez <46700662+svvyse@users.noreply.github.com>
2023-04-24 11:26:09 -0600

    Actualizar streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,19 @@
 import streamlit as st
 #import snowflake.connector 
+import numpy as np
 import pandas as pd
   
 st.title('Papeleria') 
 
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({ 'first column': [1, 2, 3, 4], 'second column': [10, 20, 30, 40] }))
+
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
 
  # connect to snowflake 
  #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
