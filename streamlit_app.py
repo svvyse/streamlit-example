85e45a15347bde0e2a57f2c5125c3513fe7c96e1
Salvador Lopez <46700662+svvyse@users.noreply.github.com>
2023-04-25 17:37:55 -0600

    Actualizar streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,6 +71,22 @@ with right_column:
     ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin")) 
   st.write(f"You are in {chosen} house!")
 
+
+import time
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
  # connect to snowflake 
  #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
  #my_cur = my_cnx.cursor() 
