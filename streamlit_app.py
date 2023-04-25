3111da76bf370ae89a5658f10443c9ef641a5441
Salvador Lopez <46700662+svvyse@users.noreply.github.com>
2023-04-25 16:49:13 -0600

    Actualizar streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,8 @@ add_slider = st.sidebar.slider(
 
 
 left_column, right_column = st.columns(2) 
-# You can use a column just like st.sidebar: left_column.button('Press me!') 
+# You can use a column just like st.sidebar: 
+left_column.button('Press me!') 
 # Or even better, call Streamlit functions inside a "with" block: 
 with right_column: 
   chosen = st.radio( 
