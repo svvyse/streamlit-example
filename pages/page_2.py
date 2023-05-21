3c4c3a2eeafb92f9b6072047ba1ca8de88c76835
Salvador Lopez <46700662+svvyse@users.noreply.github.com>
2023-05-21 13:37:15 -0600

    Update page_2.py

--- pages/page_2.py
+++ pages/page_2.py
@@ -24,8 +24,8 @@ def run_query(query):
     rows = rows.fetchall()
     return rows
 
-sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+#sheet_url = st.secrets["private_gsheets_url"]
+#rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 # Print results.
 #for row in rows:
