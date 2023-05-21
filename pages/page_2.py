2f8235ee6729f6cd40845dd73b273754919203a2
Salvador Lopez <46700662+svvyse@users.noreply.github.com>
2023-05-21 13:38:32 -0600

    Update page_2.py

--- pages/page_2.py
+++ pages/page_2.py
@@ -14,7 +14,7 @@ credentials = service_account.Credentials.from_service_account_info(
         "https://www.googleapis.com/auth/spreadsheets",
     ],
 )
-conn = connect(credentials=credentials)
+#conn = connect(credentials=credentials)
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
