8b821d72ff8f700be1968c1bb5763e71e18ddfde
Salvador Lopez <46700662+svvyse@users.noreply.github.com>
2023-05-28 12:27:22 -0600

    Update page_2.py

--- pages/page_2.py
+++ pages/page_2.py
@@ -12,36 +12,36 @@ from google.oauth2 import service_account
 from gsheetsdb import connect
 
 # Create a connection object.
-#credentials = service_account.Credentials.from_service_account_info(
-#    st.secrets["gcp_service_account"],
-#    scopes=[
-#        "https://www.googleapis.com/auth/spreadsheets",
-#    ],
-#)
-#conn = connect(credentials=credentials)
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 #@st.cache_data(ttl=600)
-#@st.cache_resource(ttl=600)
-#def run_query(query):
-#    rows = conn.execute(query, headers=1)
-#    rows = rows.fetchall()
-#    return rows
+@st.cache_resource(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
 
-#sheet_url = st.secrets["private_gsheets_url"]
-#rows = run_query(f'SELECT * FROM "{sheet_url}"')
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 # Print results.
-#for row in rows:
-#    st.write(f"{row.product_type_id} has a {row.product_type}")
+for row in rows:
+    st.write(f"{row.product_type_id} has a {row.product_type}")
 
 
-# Connect to the Google Sheet 
-sheet_id = "1m-kuvMvXRNu_M0b00dmc18-fRoxpbqmtAuhKIWV9054" 
-sheet_name = "ProductType" 
-url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}" 
-df = pd.read_csv(url, dtype=str).fillna("") 
+# Connect to the Google Sheet public sheet
+#sheet_id = "1m-kuvMvXRNu_M0b00dmc18-fRoxpbqmtAuhKIWV9054" 
+#sheet_name = "ProductType" 
+#url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}" 
+#df = pd.read_csv(url, dtype=str).fillna("") 
 # Show the dataframe (we'll delete this later) 
-st.write(df)
+#st.write(df)
 
